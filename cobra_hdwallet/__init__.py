68c3bd0c0d573e4141157971962135cac5087272
meherett <meherett@zoho.com>
2019-01-27 14:52:28 +0300

    checksum_encode function added

--- cobra_hdwallet/__init__.py
+++ cobra_hdwallet/__init__.py
@@ -18,3 +18,36 @@ from ecdsa.curves import SECP256k1
 from mnemonic.mnemonic import Mnemonic
 from two1.bitcoin.utils import rand_bytes
 from ecdsa.ecdsa import int_to_string, string_to_int
+
+
+
+
+def checksum_encode(address):
+    keccak = sha3.keccak_256()
+    out = ''
+    addr = address.lower().replace('0x', '')
+    keccak.update(addr.encode('ascii'))
+    hash_addr = keccak.hexdigest()
+    for i, c in enumerate(addr):
+        if int(hash_addr[i], 16) >= 8:
+            out += c.upper()
+        else:
+            out += c
+    return '0x' + out
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
