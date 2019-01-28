8a8b409a7d077dd789cb6933047003137a12ebd2
meherett <meherett@zoho.com>
2019-01-28 20:42:17 +0300

    check_master_key_from_mnemonic changing to static method

--- cobra_hdwallet/__init__.py
+++ cobra_hdwallet/__init__.py
@@ -135,7 +135,7 @@ class CobraHDWallet:
         return CobraHDWallet.master_key_from_seed(
             Mnemonic.to_seed(mnemonic, passphrase))
 
-    # @staticmethod
+    @staticmethod
     def check_master_key_from_mnemonic(mnemonic, language='english'):
         try:
             Mnemonic(language=language).check(mnemonic)
