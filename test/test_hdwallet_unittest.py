bda41d2b5da22119af4b7ebbb3e03311281c78b4
meherett <meherett@zoho.com>
2019-01-31 14:02:57 +0300

    test_generate_mnemonic added by unittest

--- test/test_hdwallet_unittest.py
+++ test/test_hdwallet_unittest.py
@@ -4,7 +4,19 @@ from unittest import TestCase
 
 class UnittestHDWallet(TestCase, HDWallet):
 
+    def test_generate_mnemonic(self):
 
+        enMnemonic = self.generate_mnemonic('english')
+
+        enCheck = self.check_mnemonic(enMnemonic, 'english')
+
+        self.assertTrue(enCheck)
+
+        jpMnemonic = self.generate_mnemonic('japanese')
+
+        jpCheck = self.check_mnemonic(jpMnemonic, 'japanese')
+
+        self.assertTrue(jpCheck)
 
 
 
